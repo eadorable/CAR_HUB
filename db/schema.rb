bfbdb72b1c67a83c8c73f726e2cdfb0fe32579b4
Erwin Adorable <adorable.erwin@gmail.com>
2023-10-04 16:31:39 +0200

    modify schema

--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_134633) do
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_orders_on_car_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -71,4 +80,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_134633) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cars", "users"
+  add_foreign_key "orders", "cars"
+  add_foreign_key "orders", "users"
 end
